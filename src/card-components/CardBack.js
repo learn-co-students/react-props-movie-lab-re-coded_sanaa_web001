5a1f8a383b367a64fbdb28c21a497f25e33ede86
Ahmed Aleryani <aalavyani91@yahoo.com>
2020-08-25 22:44:13 +0300

    Done.

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -12,17 +12,27 @@ export default class CardBack extends Component {
 
   generateRatingElement = () => {
     // implement meeeee! See the readme for instructions
+    if (this.props.IMDBRating !== null) {
+			return <img src={imgMapper[this.props.IMDBRating]} alt={'Image Rated: '+this.props.IMDBRating+' Out of 5'} />;
+		}
+		return <h4>No Rating Found</h4>;
   }
 
   render() {
     return (
       <div className="card-back">
-        <h3 className="title"></h3>
+        <h3 className="title">{this.props.title}</h3>
         <span />
-        { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
+        { this.generateRatingElement()}
         <span />
-        <h5 className="genres"></h5>
+        <h5 className="genres">{this.props.genres.join(", ")}</h5>
       </div>
     )
   }
 }
+// CardBack will display the title, genres and IMDB rating.
+// <CardBack />
+// 11) correctly renders the title prop
+// 12) correctly renders the genres prop as comma seperated strings
+// 13) renders 'No Rating Found' in an <h4> if the IMDBRating prop is null
+// 14) renders an <img> tag if the IMDBRating prop is valid
--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -30,14 +30,34 @@ const posterMap = {
 export default class MovieCard extends Component {
 
   render() {
+    console.log(this.props)
     return (
       <div className="movie-card">
         {/* which component should receive which props? */}
-        <CardFront />
-        <CardBack />
+     
+        <CardFront poster={posterMap[this.props.poster]}
+        />
+        <CardBack 
+        genres={this.props.genres}
+        title={this.props.title}
+        IMDBRating={this.props.IMDBRating}
+        />
       </div>
     )
   }
+ 
 }
 
+MovieCard.defaultProps = {
+  title: 'Unknown',
+  IMDBRating: null,
+  genres: ['No Genre(s) Found'],
+  poster: 'default'
+};
+
 // Don't forget your default props!
+// <MovieCard />
+// 6) assigns a default prop of 'Unknown' for the title prop
+// 7) assigns a default prop of null for the IMDBRating prop
+// 8) assigns a default prop of ['No Genre(s) Found'] for the genres prop
+// 9) assigns a default prop of 'default' for the poster prop
