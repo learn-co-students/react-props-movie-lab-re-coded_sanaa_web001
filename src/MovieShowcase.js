ce5e3a7cb257668e46c3feea111f4859d25e1351
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-08-29 09:49:49 +0300

    adding lab deliverables

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
-import MovieCard from './card-components/MovieCard.js'
-import movieData from './data.js'
+import React, { Component } from "react";
+import MovieCard from "./card-components/MovieCard.js";
+import movieData from "./data.js";
 
 export default class MovieShowcase extends Component {
-
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
-  }
+    return movieData.map((element) => (
+      <MovieCard
+        title={element.title}
+        IMDBRating={element.IMDBRating}
+        genres={element.genres}
+        poster={element.poster}
+      />
+    ));
+  };
 
   render() {
-    return (
-      <div id="movie-showcase">
-        {this.generateMovieCards()}
-      </div>
-    )
+    return <div id="movie-showcase">{this.generateMovieCards()}</div>;
   }
 }
+
+let mSC = new MovieShowcase();
+console.log(mSC.generateMovieCards());
