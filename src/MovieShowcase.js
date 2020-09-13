0d293b6788914b92f2aa69607559ea871ff3601f
OsamahGharad <osamah.gharred@gmail.com>
2020-09-13 17:58:55 +0300

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,6 +6,16 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+   const movies =movieData.map((movie,id) =>
+     <MovieCard
+          key={this.id}
+          title={this.title}
+          title={movie.title}
+          IMDBRating={movie.IMDBRating}
+          genres={movie.genres}
+          poster={movie.poster}
+    />)
+    return movies;
   }
 
   render() {
@@ -16,3 +26,8 @@ export default class MovieShowcase extends Component {
     )
   }
 }
+// ✓ renders at least one <MovieCard /> component
+// ✓ renders 9 <MovieCard /> components
+// 1) passes all 4 props to every component
+// 2) has generated the correct prop values for the first movie
+// 3) has generated the correct prop values for the last movie
